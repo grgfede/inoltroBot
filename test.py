2f42f5524dbac3fb099c70e3dcb7c4dd751e05f4
grgfede <federico.frari1995@gmail.com>
2025-06-08 15:29:08 +0200

    test

---
+++ test.py
@@ -0,0 +1,11 @@
+﻿from aiohttp import web
+
+async def handle(request):
+    return web.Response(text="OK")
+
+app = web.Application()
+app.router.add_get("/", handle)
+
+if __name__ == "__main__":
+    print("Starting app on port 8080")
+    web.run_app(app, port=8080)
